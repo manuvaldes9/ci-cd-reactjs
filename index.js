d9cc5f2289d6c0e408307f762461c41108898bb4
manuvaldes9 <manuelvaldes96@gmail.com>
2021-06-29 12:02:38 -0300

    Cambios en Dockerfile e index.js

--- index.js
+++ index.js
@@ -10,12 +10,18 @@ const port = process.env.PORT || 8000
 
 app.get('/', (req,res) => res.send('hello world'))
 
-app.listen(port, () => console.log(port))
+//app.listen(port, () => console.log(port))
 
+async function main (){
+    await app.listen(port);
+    console.log('Server is running')
+}
+
+main();
 var a = 3;
 var b = 5;
 
 console.log(`Add ${a} + ${b} = ${principal.add(a,b)}`);
-console.log(`Minus ${a} + ${b} = ${principal.minus(a,b)}`);
-console.log(`Multiply ${a} + ${b} = ${principal.multiply(a,b)}`);
-console.log(`Divide ${a} + ${b} = ${principal.divide(a,b)}`);
+console.log(`Minus ${a} - ${b} = ${principal.minus(a,b)}`);
+console.log(`Multiply ${a} * ${b} = ${principal.multiply(a,b)}`);
+console.log(`Divide ${a} / ${b} = ${principal.divide(a,b)}`);
