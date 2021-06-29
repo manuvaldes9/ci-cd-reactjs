5359b4c93ec8c993de9c3b9a0dacd7b9258b5b54
manuvaldes9 <manuelvaldes96@gmail.com>
2021-06-29 10:39:43 -0300

    Se agrega express + Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+from node:12
+
+WORKDIR /app
+
+COPY package*.json ./
+
+RUN npm install
+
+COPY . .
+
+CMD ["npm","start"]
--- index.js
+++ index.js
@@ -2,6 +2,16 @@
 
 var principal = require('./app/principal');
 
+const express = require('express')
+
+const app = express()
+
+const port = process.env.PORT || 8000
+
+app.get('/', (req,res) => res.send('hello world'))
+
+app.listen(port, () => console.log(port))
+
 var a = 3;
 var b = 5;
 
