951867299218b8db86414320446b43232439225a
manuvaldes9 <manuelvaldes96@gmail.com>
2021-06-30 00:31:10 -0300

    Cambios en travis.yml

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express()
 
 const port = process.env.PORT || 8000
 
-app.get('/', (req,res) => res.send('hello world'))
+app.get('/', (req,res) => res.send('hello world!'))
 
 //app.listen(port, () => console.log(port))
 
